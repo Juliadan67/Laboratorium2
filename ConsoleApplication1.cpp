f006b946037b1c5679edeb83c5b5fb3a3b35f972
Juliadan67 <jdanielewicz.jd@gmail.com>
2025-10-16 16:07:42 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+using namespace std;
+
+//lab2 zadanie 1
+/*int main() {
+	for (int i = 0; i < 10 ; i++) {
+		cout << i << endl;
+	}
+	return 0;
+}*/
+
+/*int main() {
+	for (int i = 0; i <= 10; i++) {
+		cout << i << endl;
+	}
+	return 0;
+}*/
+
+/*int main() {
+	for (int i = 5; i <= 7; i++) {
+		cout << i << endl;
+	}
+	return 0;
+}*/
+
+/*int main() {
+	for (int i = 5; i < 7; i++) {
+		cout << i << endl;
+	}
+	return 0;
+}*/
+
+/*int main() {
+	for (int i = 0; i <= 10; i+=2) {
+		cout << i << endl;
+	}
+	return 0;
+}*/
+
+/*int main() {
+	for (int i = 10; i > 0; i--) {
+		cout << i << endl;
+	}
+	return 0;
+}*/
+
+//zadanie 2
+/*#include <stdio.h>
+int main() {
+	int fahr, celsius;
+	int start, limit, krok;
+
+	start = 0;
+	limit = 200;
+	krok = 20;
+
+    fahr = start;
+	while (fahr <= limit) {
+		celsius = 5 * (fahr - 32) / 9;
+		cout << fahr << "\t" << celsius << endl;
+		fahr = fahr + krok;
+	}
+}*/
+
+//zadanie 3
+/*#include <stdio.h>
+int main() {
+	int fahr, celsius;
+	int start, limit, krok;
+
+	start = 0;
+	limit = 200;
+	krok = 20;
+
+	fahr = start;
+	for (fahr = 0.0; fahr <= limit; fahr = fahr + krok) 
+	{
+		float celsius = (5.0 / 9.0) * (fahr - 32.0);
+		cout << fahr << "\t" << celsius << endl;
+	}
+}*/
+
+//zadanie 5
+/*#include <stdio.h>
+int main() {
+	int fahr, celsius;
+	int start, limit, krok;
+
+	start = 0;
+	limit = 200;
+	krok = 20;
+
+	fahr = start;
+	for (fahr = 0.0; fahr <= limit; fahr = fahr + krok)
+	{
+		float celsius = (5/9) * (fahr - 32.0);
+		cout << fahr << "\t" << celsius << endl;
+	}
+}*/
+
+//zadanie 6
+/*#include <stdio.h>
+int main() {
+	int fahr, celsius;
+	int start, limit, krok;
+
+	start = 0;
+	limit = 200;
+	krok = 20;
+
+	fahr = start;
+	for (fahr = 0.0; fahr <= limit; fahr = fahr + krok)
+	{
+		float celsius = (5.0 / 9.0) * (fahr - 32.0);
+		cout << fahr << "\t" << celsius << endl;
+	}
+}*/
